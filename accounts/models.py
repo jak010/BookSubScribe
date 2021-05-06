a6049b8f3a8c27d98eab75e0e609b1ebdadd6468
JianKo <bluetoon@naver.com>
2021-05-06 10:34:34 -0400

    Publisher User Model INTIALIZED

--- accounts/models.py
+++ accounts/models.py
@@ -1,23 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from .manager import UserManger
 
 
-# Create your models here.
-
-class User(AbstractBaseUser):
-    # 앞에는 DB에 저장되는 값
-    # 뒤에는 페이지나 폼에서 표시하는 값
-    DEFINE_USER_TYPE = (
+# 앞에는 DB에 저장되는 값
+# 뒤에는 페이지나 폼에서 표시하는 값
+class TypeDefine(object):
+    USER_TYPES = (
         ("Publisher", "Publisher"),
         ("Subscriber", "Subscriber")
     )
 
+# Create your models here.
+
+class User(AbstractBaseUser):
     email = models.EmailField(max_length=50, verbose_name="User Email", unique=True)
     uuid = models.UUIDField(max_length=32, editable=False, verbose_name="User UUID")
-    type = models.CharField(max_length=20, choices=DEFINE_USER_TYPE, default=DEFINE_USER_TYPE[1][1])
+    type = models.CharField(max_length=20, choices=TypeDefine.USER_TYPES, default=TypeDefine.USER_TYPES[1][1])
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    objects = UserManger()
+
     def __str__(self):
         return self.email
+
+
+class PublisherManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return super().get_queryset(*args, **kwargs).filter(type=TypeDefine.USER_TYPES[0][0])
+
+
+class Publisher(User):
+    objects = PublisherManager()
+
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.type = TypeDefine.USER_TYPES[0][0]
+        return super().save(*args, **kwargs)
+
+
+class PublisherAttribute(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pub_name = models.CharField(max_length=50, verbose_name="Publisher Name")
+    create_time = models.DateTimeField(auto_now_add=True)  # 레코드 생성 시 시간 자동 저장
+    modify_time = models.DateTimeField(auto_now=True)  # 레코드 갱신 시 현재 시간 저장
---
+++ book/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from bpm.settings import AUTH_USER_MODEL
+
+
+# Create your models here.
